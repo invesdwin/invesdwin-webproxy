9c91f58308326466ac17419282963d99a118c20f
subes <gsubes@gmail.com>
2018-05-09 16:13:37 +0200

    abort on too many dom listeners memory leak

--- invesdwin-webproxy-parent/invesdwin-webproxy/src/main/java/de/invesdwin/webproxy/internal/get/page/WebClientWorker.java
+++ invesdwin-webproxy-parent/invesdwin-webproxy/src/main/java/de/invesdwin/webproxy/internal/get/page/WebClientWorker.java
@@ -1,17 +1,24 @@
 package de.invesdwin.webproxy.internal.get.page;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Method;
 import java.net.URI;
+import java.util.List;
 
 import javax.annotation.concurrent.ThreadSafe;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomChangeListener;
+import com.gargoylesoftware.htmlunit.html.DomNode;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import de.invesdwin.context.log.Log;
 import de.invesdwin.util.assertions.Assertions;
+import de.invesdwin.util.lang.Reflections;
 import de.invesdwin.util.lang.uri.URIs;
 import de.invesdwin.util.time.Instant;
 import de.invesdwin.util.time.duration.Duration;
@@ -23,6 +30,19 @@ import de.invesdwin.webproxy.internal.get.ADownloadWorker;
 
 @ThreadSafe
 public class WebClientWorker extends ADownloadWorker<Page, GetPageConfig> {
+
+    private static final int MAX_DOM_LISTENERS = 100_000;
+    private static final MethodHandle DOM_LISTENERS_GETTER;
+
+    static {
+        final Method safeGetDomListenersMethod = Reflections.findMethod(DomNode.class, "safeGetDomListeners");
+        Reflections.makeAccessible(safeGetDomListenersMethod);
+        try {
+            DOM_LISTENERS_GETTER = MethodHandles.lookup().unreflect(safeGetDomListenersMethod);
+        } catch (final IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private final Log log = new Log(this);
     private final WebClient client;
@@ -56,7 +76,8 @@ public class WebClientWorker extends ADownloadWorker<Page, GetPageConfig> {
             boolean timeExpired;
             do {
                 timeExpired = new Duration(start).isGreaterThan(callback.getMaxDelay());
-                if (timeExpired || callback.isDownloadFinished(client, page)) {
+                if (timeExpired || callback.isDownloadFinished(client, page)
+                        || abortOnTooManyDomListeners(callback, page)) {
                     if (timeExpired) {
                         log.warn("%s MaxDelay of %s expired on %s", callback.getClass().getName(),
                                 callback.getMaxDelay(), page.getWebResponse().getWebRequest().getUrl());
@@ -67,6 +88,26 @@ public class WebClientWorker extends ADownloadWorker<Page, GetPageConfig> {
                 }
             } while (!timeExpired);
         }
+    }
+
+    private boolean abortOnTooManyDomListeners(final JavascriptWaitCallback callback, final Page page) {
+        if (page instanceof DomNode) {
+            final DomNode cPage = (DomNode) page;
+            try {
+                final List<DomChangeListener> domListeners = (List<DomChangeListener>) DOM_LISTENERS_GETTER
+                        .invoke(cPage);
+                if (domListeners.size() > MAX_DOM_LISTENERS) {
+                    log.warn(
+                            "%s max dom listeners of %s exceeded on %s [%s]. Aborting because otherwise a memory overflow will happen.",
+                            callback.getClass().getName(), MAX_DOM_LISTENERS,
+                            page.getWebResponse().getWebRequest().getUrl(), domListeners.size());
+                    return true;
+                }
+            } catch (final Throwable e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return false;
     }
 
     @Override
