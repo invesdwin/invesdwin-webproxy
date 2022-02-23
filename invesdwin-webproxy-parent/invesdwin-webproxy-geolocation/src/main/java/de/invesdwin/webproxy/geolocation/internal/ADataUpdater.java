2a6ec62fd0d8ec50083f4cbc5f64302f030e27ce
Edwin Stang <gsubes@gmail.com>
2022-02-23 12:17:05 +0100

    PER-211 add ability to do post requests in URIsConnect

--- invesdwin-webproxy-parent/invesdwin-webproxy-geolocation/src/main/java/de/invesdwin/webproxy/geolocation/internal/ADataUpdater.java
+++ invesdwin-webproxy-parent/invesdwin-webproxy-geolocation/src/main/java/de/invesdwin/webproxy/geolocation/internal/ADataUpdater.java
@@ -93,7 +93,7 @@ public abstract class ADataUpdater {
                 @Override
                 protected InputStream innerNewDelegate() {
                     try {
-                        return URIs.connect(sourceUrl).getInputStream();
+                        return URIs.connect(sourceUrl).downloadInputStream();
                     } catch (final IOException e) {
                         throw new TransparentRuntimeIOExeption(e);
                     }
