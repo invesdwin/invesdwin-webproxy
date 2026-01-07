9ea4da21833e8f3c0fcacf1cf5aaea92d71c2c36
Edwin Stang <gsubes@gmail.com>
2026-01-07 12:07:42 +0100

    Revert "PER-1068 make sure info is retained whether the stub gets enabled or"
    This reverts commit 70b742f3d223b2257f507e5bbf3c459123080269.

--- invesdwin-webproxy-parent/invesdwin-webproxy-broker-contract/src/main/java/de/invesdwin/webproxy/broker/contract/BrokerServiceStub.java
+++ invesdwin-webproxy-parent/invesdwin-webproxy-broker-contract/src/main/java/de/invesdwin/webproxy/broker/contract/BrokerServiceStub.java
@@ -1,6 +1,9 @@
 package de.invesdwin.webproxy.broker.contract;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.annotation.concurrent.ThreadSafe;
 
@@ -9,21 +12,35 @@ import de.invesdwin.context.test.ATest;
 import de.invesdwin.context.test.ITestContextSetup;
 import de.invesdwin.context.test.stub.StubSupport;
 import de.invesdwin.util.assertions.Assertions;
+import de.invesdwin.util.collections.Collections;
+import de.invesdwin.util.collections.Iterables;
 import de.invesdwin.webproxy.broker.contract.internal.BrokerContextLocation;
-import de.invesdwin.webproxy.broker.contract.internal.InMemoryBrokerService;
+import de.invesdwin.webproxy.broker.contract.schema.BrokerRequest.AddToBeVerifiedProxiesRequest;
+import de.invesdwin.webproxy.broker.contract.schema.BrokerRequest.ProcessResultFromCrawlerRequest;
+import de.invesdwin.webproxy.broker.contract.schema.BrokerResponse.GetTaskForCrawlerResponse;
+import de.invesdwin.webproxy.broker.contract.schema.BrokerResponse.GetWorkingProxiesResponse;
+import de.invesdwin.webproxy.broker.contract.schema.Proxy;
+import de.invesdwin.webproxy.broker.contract.schema.RawProxy;
 import jakarta.inject.Named;
 
 @Named
 @ThreadSafe
-public class BrokerServiceStub extends StubSupport {
+public class BrokerServiceStub extends StubSupport implements IBrokerService {
 
     private static volatile boolean enabled = true;
 
     private static volatile boolean crawlForProxies;
 
+    private final Map<RawProxy, Proxy> proxies = new ConcurrentHashMap<RawProxy, Proxy>();
+    private final Set<RawProxy> rawProxies = Collections.newSetFromMap(new ConcurrentHashMap<RawProxy, Boolean>());
+
+    public static void setCrawlForProxies(final boolean value) {
+        crawlForProxies = value;
+    }
+
     @Override
     public void setUpContextLocations(final ATest test, final List<PositionedResource> locations) throws Exception {
-        if (!BrokerServiceStub.isEnabled()) {
+        if (!enabled) {
             if (!locations.remove(BrokerContextLocation.SERVICE_CONTEXT)) {
                 Assertions.checkTrue(locations.remove(BrokerContextLocation.SERVICE_CONTEXT_FALLBACK));
             }
@@ -33,8 +50,61 @@ public class BrokerServiceStub extends StubSupport {
 
     @Override
     public void setUpContext(final ATest test, final ITestContextSetup ctx) {
-        if (BrokerServiceStub.isEnabled()) {
-            ctx.replaceBean(IBrokerService.class, InMemoryBrokerService.class);
+        if (enabled) {
+            ctx.replaceBean(IBrokerService.class, this.getClass());
+        } else {
+            ctx.deactivateBean(this.getClass());
+        }
+    }
+
+    @Override
+    public GetWorkingProxiesResponse getWorkingProxies() {
+        final GetWorkingProxiesResponse response = new GetWorkingProxiesResponse();
+        response.getWorkingProxies().addAll(proxies.values());
+        return response;
+    }
+
+    @Override
+    public void addToBeVerifiedProxies(final AddToBeVerifiedProxiesRequest request) {
+        rawProxies.addAll(request.getToBeVerifiedProxies());
+    }
+
+    @Override
+    public GetTaskForCrawlerResponse getTaskForCrawler() {
+        final GetTaskForCrawlerResponse response = new GetTaskForCrawlerResponse();
+        response.setCrawlForProxies(crawlForProxies);
+        if (response.isCrawlForProxies()) {
+            BrokerServiceStub.crawlForProxies = false;
+        } else {
+            for (final RawProxy rawProxy : Iterables.limit(rawProxies, BrokerContractProperties.MAX_PROXIES_PER_TASK)) {
+                response.getToBeVerifiedProxies().add(rawProxy);
+                rawProxies.remove(rawProxy);
+            }
+        }
+        for (int i = 1; i < 100; i++) {
+            response.getToBeScannedPorts().add(i);
+        }
+        return response;
+    }
+
+    @Override
+    public void processResultFromCrawler(final ProcessResultFromCrawlerRequest request) {
+        addProxies(request.getSuccessfullyVerifiedProxies());
+        removeProxies(request.getUnsuccessfullyVerifiedProxies());
+    }
+
+    private void addProxies(final List<Proxy> add) {
+        for (final Proxy proxy : add) {
+            final RawProxy raw = new RawProxy();
+            raw.setHost(proxy.getHost());
+            raw.setPort(proxy.getPort());
+            proxies.put(raw, proxy);
+        }
+    }
+
+    private void removeProxies(final List<RawProxy> remove) {
+        for (final RawProxy rawProxy : remove) {
+            proxies.remove(rawProxy);
         }
     }
 
@@ -45,13 +115,4 @@ public class BrokerServiceStub extends StubSupport {
     public static boolean isEnabled() {
         return enabled;
     }
-
-    public static void setCrawlForProxies(final boolean value) {
-        crawlForProxies = value;
-    }
-
-    public static boolean isCrawlForProxies() {
-        return crawlForProxies;
-    }
-
 }
